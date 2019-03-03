e4c8dd1a06a226bace1a94aa708fa76f735d68bb
Connor Matza <connormatza@student.olympic.edu>
2019-03-03 15:45:41 -0800

    Deploy : deploy branch

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const Config = {
+  apiKey: "AIzaSyCTqd88YNv1R_jQhXmg2J60biiKKbYjAnU",
+  authDomain: "firedux-todo-e1cc7.firebaseapp.com",
+  databaseURL: "https://firedux-todo-e1cc7.firebaseio.com",
+  projectId: "firedux-todo-e1cc7",
+  storageBucket: "firedux-todo-e1cc7.appspot.com",
+  messagingSenderId: "149505582906"
+
+}
+
+export default Config;
