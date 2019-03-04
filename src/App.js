c5423b3a85dbc605c72fb3711840c65b8f00a15d
Connor Matza <connormatza@student.olympic.edu>
2019-03-03 16:43:36 -0800

    More comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import List from './components/List';
 import "./styles/styles.css"
+
 class App extends Component {
   render() {
     return (
---
+++ src/publicconfig.js
@@ -0,0 +1,6 @@
+const Config = {
+  //Place your firebase Real Time database config in this block.
+  //Then rename the file config.js.
+}
+
+export default Config;
